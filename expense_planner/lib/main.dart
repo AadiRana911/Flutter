6c4fa20294008c5a0b941488caadd814ea95def1
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-03-24 20:49:58 +0500

    Initializing Expense Planner Application

---
+++ expense_planner/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Planner'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Container(
+              child: Text('Chart'),
+              width: double.infinity,
+            )
+          ),
+          Card(
+            child: Text('List of Transactions')
+          )
+        ],
+      ),
+    );
+  }
+}

c161adbe0067b0b5980e6e2ce50bd22ccb7b138a
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-03-26 16:11:32 +0500

    Adding a list of transactions

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +18,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1', 
+      title: 'New Shoes', 
+      amount: 69.99,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2', 
+      title: 'Weekly Groceries', 
+      amount: 35.76,
+      date: DateTime.now()
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
 

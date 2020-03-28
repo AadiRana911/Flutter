69b9a53553b8c72bacc7f8ced4bdcf13a587d276
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-03-28 23:25:39 +0500

    Formatting & styling inputs and outputs

--- expense_planner/lib/widgets/new_transaction.dart
+++ expense_planner/lib/widgets/new_transaction.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {  
   final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
   NewTransaction(this.addTx);
 
+  void submitData(){
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if(enteredTitle.isEmpty || enteredAmount <= 0){
+      return;
+    }
+    addTx(enteredTitle, enteredAmount);
+  }
   @override
   Widget build(BuildContext context) {
-    final titleController = TextEditingController();
-    final amountController = TextEditingController();
+    
     return Card(
             elevation: 5,
             child: Container(
@@ -18,15 +27,18 @@ class NewTransaction extends StatelessWidget {
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
                     controller: titleController,
+                    onSubmitted: (_) => submitData(),
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
                     controller: amountController,
+                    keyboardType: TextInputType.number,
+                    onSubmitted: (_) => submitData(),
                   ),
                   FlatButton(
                     textColor: Colors.purple, 
                     child: Text('Add Transaction'),
-                    onPressed: () => addTx(titleController.text, double.parse(amountController.text))
+                    onPressed: submitData 
                     
                   )
                 ],

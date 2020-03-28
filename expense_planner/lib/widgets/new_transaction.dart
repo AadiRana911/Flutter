5b19cfa9a4e1aa18d01c00a53d433c3604eee3c1
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-03-29 02:40:44 +0500

    Fixing Minor bugs in modal bottom sheet

--- expense_planner/lib/widgets/new_transaction.dart
+++ expense_planner/lib/widgets/new_transaction.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {  
+class NewTransaction extends StatefulWidget {
   final Function addTx;
+  NewTransaction(this.addTx);
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+class _NewTransactionState extends State<NewTransaction> {  
   final titleController = TextEditingController();
   final amountController = TextEditingController();
-  NewTransaction(this.addTx);
+
 
   void submitData(){
     final enteredTitle = titleController.text;
@@ -12,7 +17,8 @@ class NewTransaction extends StatelessWidget {
     if(enteredTitle.isEmpty || enteredAmount <= 0){
       return;
     }
-    addTx(enteredTitle, enteredAmount);
+    widget.addTx(enteredTitle, enteredAmount);
+    Navigator.of(context).pop();
   }
   @override
   Widget build(BuildContext context) {
